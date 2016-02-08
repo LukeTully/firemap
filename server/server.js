11852f8429e77af633c1970c1b59982254554a51
Luke Tully <luke@webrenos.com>
2016-02-08 15:48:09 -0800

    Fixed hanging server response issue

--- server/server.js
+++ server/server.js
@@ -11,36 +11,40 @@ var app = express();
 var baseUrl = "/computermapping/fires";
 
 var connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'webrenos',
-  password: '2o;2R297_1Ny',
-  database: 'fire',
-  port: '3306'
+    host: '127.0.0.1',
+    user: 'webrenos',
+    password: '2o;2R297_1Ny',
+    database: 'fire',
+    port: '3306'
 });
 connection.connect();
 app.use(compress({threshold: 1}));
 app.use("/computermapping", express.static(__dirname + '/static'));
 
-app.get( baseUrl, function ( req, res ) {
-res.set('Access-Control-Allow-Origin','*');
- 
-  if (parseInt(req.query.date) > 1949 && parseInt(req.query.date) < 2013) {
-    var date = parseInt(req.query.date);
+app.get(baseUrl, function (req, res) {
+    res.set('Access-Control-Allow-Origin', '*');
 
-  } else if (req.query.srcagency) {
-    connection.query("SELECT * FROM fires WHERE YEAR_ = ? AND SRC_AGENCY = ?", [date, req.query.srcagency], function (err, rows, fields) {
-      res.send(JSON.stringify(rows));
-    });
-  } else {
-    connection.query("SELECT * FROM fires WHERE YEAR_ = ?", [date], function (err, rows, fields) {
-      if (rows.length === 0) {
-        res.send({errors: ["Please enter a valid date"]});
-      }
-      res.send(JSON.stringify(rows));
-    });
+    if (parseInt(req.query.date) > 1949 && parseInt(req.query.date) < 2013) {
+        var date = parseInt(req.query.date);
 
-  }
+        connection.query("SELECT * FROM fires WHERE YEAR_ = ?", [date], function (err, rows, fields) {
+            if (rows.length === 0) {
+                res.send({errors: ["Please enter a valid date"]});
+            } else if (req.query.srcagency) {
+                connection.query("SELECT * FROM fires WHERE YEAR_ = ? AND SRC_AGENCY = ?", [date, req.query.srcagency], function (
+                    err,
+                    rows,
+                    fields) {
+                    res.send(JSON.stringify(rows));
+                });
+            }
+            res.send(JSON.stringify(rows));
+        });
+
+
+    }
+
 });
 
 
-app.listen("3000"); //Needs to be changed on deployment
+app.listen("3679"); //Needs to be changed on deployment
