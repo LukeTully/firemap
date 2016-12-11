feac1e7d758d01e6cc44109e6bf5539043f11347
Luke Tully <luke@webrenos.com>
2016-12-10 19:12:48 -0800

    Removed legacy comments

--- server/static/scripts/directives/directives.js
+++ server/static/scripts/directives/directives.js
@@ -1,105 +1,83 @@
 angular.module('mapprojectApp')
     .directive('ltMap', () => ({
-          restrict: 'E',
-          scope: {
-              coordList: '=coords',
-              currentPos: '=position',
-            },
-          link (scope, element, attrs) {
-              let map = L.map('map').setView([51.106739, -114.913696], 4);
-              L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
+        restrict: 'E',
+        scope: {
+            coordList: '=coords',
+            currentPos: '=position',
+        },
+        link(scope, element, attrs) {
+            let map = L.map('map').setView([51.106739, -114.913696], 4);
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {}).addTo(map);
 
 
-              let mapConfig = {
-                  min: 0,
-                  max: null,
-                  group: new L.LayerGroup(),
-                  canvasLayer: L.canvas(),
-                  pointSet: [],
-                  pointSetAlreadyAdded: [],
-                  map,
-                  currentLat: '',
-                  currentLong: '',
-                };
-              scope.$watch(attrs.position, (newVal, oldVal) => {
-                  if (newVal.latitude !== '') {
-                      mapConfig.currentLat = newVal.latitude;
-                      mapConfig.currentLong = newVal.longitude;
-                      let latlng = L.latLng(mapConfig.currentLat, mapConfig.currentLong);
-                      L.circle(latlng, 200).addTo(mapConfig.map);
-                      mapConfig.map.setView(latlng);
+            let mapConfig = {
+                min: 0,
+                max: null,
+                group: new L.LayerGroup(),
+                canvasLayer: L.canvas(),
+                pointSet: [],
+                pointSetAlreadyAdded: [],
+                map,
+                currentLat: '',
+                currentLong: '',
+            };
+            scope.$watch(attrs.position, (newVal, oldVal) => {
+                if (newVal.latitude !== '') {
+                    mapConfig.currentLat = newVal.latitude;
+                    mapConfig.currentLong = newVal.longitude;
+                    let latlng = L.latLng(mapConfig.currentLat, mapConfig.currentLong);
+                    L.circle(latlng, 200).addTo(mapConfig.map);
+                    mapConfig.map.setView(latlng);
+                }
+            }, true);
+
+
+            scope.$watch('coordList', (newVal, oldVal) => {
+                data = newVal;
+
+                console.log('Triggered');
+                if (newVal) {
+                    console.log('Aggregating');
+
+                    if (mapConfig.max) {
+                        data = data.splice(mapConfig.min, mapConfig.max);
                     }
-                }, true);
-
-
-              scope.$watch('coordList', (newVal, oldVal) => {
-                  data = newVal;
-
-                  console.log('Triggered');
-                  if (newVal) {
-                      console.log('Aggregating');
-//		var geoProvs = L.geoJson(provinces, {
-//			style: {
-//				color: "#000000",
-//				weight: 1,
-//				fill: false
-//			}
-//		});
-
-//		geoProvs.addTo(map);
-
-//
-//		var markers = new L.marker,
-//				potholePriority = [],
-//				u = 0;
-
-// TODO: Loop and add markers here
-
-// Match priority value
-//		var priorityRegex = /Priority (\d)/;
-//		var holes = potholes.features[0].items;
-
-
-                      if (mapConfig.max) {
-                          data = data.splice(mapConfig.min, mapConfig.max);
-                        }
-                      if (mapConfig.group.getLayers()) {
-                          console.log('has layers');
-                          mapConfig.map.removeLayer(mapConfig.canvasLayer);
-                          mapConfig.canvasLayer = L.canvas();
-                        }
-                      console.log('Cleared');
-                      aggregatePoint(data, mapConfig, aggregateCB);
+                    if (mapConfig.group.getLayers()) {
+                        console.log('has layers');
+                        mapConfig.map.removeLayer(mapConfig.canvasLayer);
+                        mapConfig.canvasLayer = L.canvas();
                     }
-                });
-            },
-        }));
+                    console.log('Cleared');
+                    aggregatePoint(data, mapConfig, aggregateCB);
+                }
+            });
+        },
+    }));
 
 
 function aggregatePoint(dataList, config, cb) {
     console.log(config);
     console.timeEnd('Cheese');
-    // debugger;
-    // check if this is an array of arrays
     if (isArray(dataList)) {
-      if (isArray(dataList[0])) {
-          console.log('Looping started');
-          loopOverArrayForPoints(dataList[0], config, (config) => {
-              console.log('Looping Ended');
-              dataList.splice(0, 1);
-              cb(dataList, config);
-          });
-      } 
-          console.log('Looping started');
-          loopOverArrayForPoints(dataList, config, (config) => {
-              console.log('Looping Ended');
+        if (isArray(dataList[0])) {
+            console.log('Looping started');
+            loopOverArrayForPoints(dataList[0], config, (config) => {
+                console.log('Looping Ended');
+                dataList.splice(0, 1);
+                cb(dataList, config);
+            });
+        }
+        console.log('Looping started');
+        loopOverArrayForPoints(dataList, config, (config) => {
+            console.log('Looping Ended');
 
-              cb(dataList, config);
-          });
-      }
-  } e {
-      throw 'datalist was not an array';
-  }
+            cb(dataList, config);
+        });
+    }
+}
+e {
+    throw 'datalist was not an array';
+}
 }
 
 fuconstion pushIntoPointSet(point, config, cb) {
@@ -110,95 +88,95 @@ fuconstion pushIntoPointSet(point, config, cb) {
     //			console.log(marker.intensity);
     config.pointSet.push(marker); // Push the newly created marker onto the stack
     if (point.last == true) {
-      // var group = new L.LayerGroup();
+        // var group = new L.LayerGroup();
         for (let i = 0; i < config.pointSet.length; i++) {
             //						debugger;
-          //						pointSet[i].addTo(map);
+            //						pointSet[i].addTo(map);
             geoJson(confg.pointSet[i], {
                 constToLayer(feature, latlng) {
-                constet intensity = parseInt(config.pointSet[i].intensity);
-                constet intensityRadius = intensity / 2;
+                    constet intensity = parseInt(config.pointSet[i].intensity);
+                    constet intensityRadius = intensity / 2;
                     t markerOptions = {
-                      stroke: false,
-                      clickable: true,
-                      fillOpacity: 1,
-                      renderer: config.canvasLayer,
-                      radius: intensityRadius,
-                      fillColor: '#671c03',
+                        stroke: false,
+                        clickable: true,
+                        fillOpacity: 1,
+                        renderer: config.canvasLayer,
+                        radius: intensityRadius,
+                        fillColor: '#671c03',
                     };
                     // Colour options from lowest to highest hectares burned
 
-                
-                  config.group.addLayer(new L.circle(latlng, intensityRadius, markerOptions));
-                  return L.circleMarker(latlng, markerOptions);
-              },
-          });
+
+                    config.group.addLayer(new L.circle(latlng, intensityRadius, markerOptions));
+                    return L.circleMarker(latlng, markerOptions);
+                },
+            });
         }
-    
-      config.pointSetAlreadyAdded.push(config.pointSet);
-      config.pointSet = [];
+
+        config.pointSetAlreadyAdded.push(config.pointSet);
+        config.pointSet = [];
         cb(config.group, config);
-      //				debugger;
+        //				debugger;
     }
 }
 
 // :TODO Strip this callback out entirely
 function aggregateCB(cheese) {
     if (isArray(cheese) && cheese.length !== 0) {
-      //				debugger;
-      console.log('Callback called');
+        //				debugger;
+        console.log('Callback called');
         console.time('Cheese');
-      //				var newTime = window.setTimeout(aggregatePoint, 1, cheese, aggregateCB);
-    else {
-      console.log('Done');
-    }
-}
-
-function isArray(arr) {
-     (arr !== undefined && arr !== null) {
-         (arr.pop && typeof arr.pop === 'function') {
-        n true;
+        //				var newTime = window.setTimeout(aggregatePoint, 1, cheese, aggregateCB);
         else {
-          return false;
-      }
+            console.log('Done');
+        }
     }
-}
+
+    function isArray(arr) {
+        (arr !== undefined && arr !== null) {
+            (arr.pop && typeof arr.pop === 'function') {
+                n true;
+                else {
+                    return false;
+                }
+            }
+        }
 
 
-function loopOverArrayForPoints(arr, config, cb) {
-    for (let i = 0; i < arr.length; i++) {
-        // Set the current object for reference later
-        //				this.currentSet = arr[i];
+        function loopOverArrayForPoints(arr, config, cb) {
+            for (let i = 0; i < arr.length; i++) {
+                // Set the current object for reference later
+                //				this.currentSet = arr[i];
 
-      // TODO: Write a more extensive test for this.
-         (parseInt(arr[i].LATITUDE) !== 0 && parseInt(arr[i].LONGITUDE) !== 0) {
-            if (i > 50000) {
-              //						console.log(i);
-          }
-             (i == arr.length - 1) {
-                shIntoPointSet({
-                  lat: arr[i].LATITUDE,
-                  long: arr[i].LONGITUDE,
-                  intensity: arr[i].SIZE_HA,
-                  last: true,
-                }, config, (group, config) => {
-                    config.map.addLayer(group);
-                    console.log('Added Points to map');
-              });
-            One of a few spots that doesn't need to be sent the config
-            else {
-                shIntoPointSet({
-                  lat: arr[i].LATITUDE,
-                  long: arr[i].LONGITUDE,
-                  intensity: arr[i].SIZE_HA,
-                  last: false,
-              }, config);
-        
-        else {
-          console.log('Could not parse proper coords from this object:');
-          console.log(arr[i]);
-          arr.splice(i, 1);
-          i -= 2;
-      }
-    }
-}
+                // TODO: Write a more extensive test for this.
+                (parseInt(arr[i].LATITUDE) !== 0 && parseInt(arr[i].LONGITUDE) !== 0) {
+                    if (i > 50000) {
+                        //						console.log(i);
+                    }
+                    (i == arr.length - 1) {
+                        shIntoPointSet({
+                            lat: arr[i].LATITUDE,
+                            long: arr[i].LONGITUDE,
+                            intensity: arr[i].SIZE_HA,
+                            last: true,
+                        }, config, (group, config) => {
+                            config.map.addLayer(group);
+                            console.log('Added Points to map');
+                        });
+                        One of a few spots that doesn 't need to be sent the config
+                        else {
+                            shIntoPointSet({
+                                lat: arr[i].LATITUDE,
+                                long: arr[i].LONGITUDE,
+                                intensity: arr[i].SIZE_HA,
+                                last: false,
+                            }, config);
+
+                            else {
+                                console.log('Could not parse proper coords from this object:');
+                                console.log(arr[i]);
+                                arr.splice(i, 1);
+                                i -= 2;
+                            }
+                        }
+                    }
