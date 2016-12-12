067d9c1fb6939d41c949a8b2ce18eccc08df5230
Luke Tully <luke@webrenos.com>
2016-12-12 03:11:55 -0800

    Modernized syntax and added basic date validation

--- server/static/scripts/services/services.js
+++ server/static/scripts/services/services.js
@@ -2,69 +2,66 @@
  * Created by Luke on 11-22-2014.
  */
 angular.module('mapprojectApp').service('FireQuery', ($http) => {
-  return function () {
-    let date = 1950;
-    let agency = '';
-    this.getFires = function getFires(cb) {
-      this.domain = 'http://obsessively.ca';
-      this.apiString = 'computermapping/fires';
-      this.url = `${this.domain}/${this.apiString}`;
-      this.fullUrl = this.url;
-      if (date) {
-        this.fullUrl += `?date=${date}`;
-      } else {
-        data = [];
-        return data;
-      }
-      if (date && agency) {
-        this.fullUrl += `&srcagency=${agency}`;
-      }
-      $http.get(this.fullUrl, { cache: true })
-        .success(( data, status, headers, config ) => {
+  let date = 1950;
+  let agency = '';
+  this.getFires = (cb) => {
+    this.domain = 'http://obsessively.ca';
+    this.apiString = 'computermapping/fires';
+    this.url = `${this.domain}/${this.apiString}`;
+    this.fullUrl = this.url;
+
+    /* If there is a valid value assigned to date, append it to the URL */
+    if (!isNaN(data)) {
+      this.fullUrl += `?date=${date}`;
+    }
+
+    if (date && agency) {
+      this.fullUrl += `&srcagency=${agency}`;
+    }
+    $http.get(this.fullUrl, { cache: true })
+        .success((data, status, headers, config) => {
           cb(data);
         })
-        .error(( data, status, headers, config ) => {
+        .error((data, status, headers, config) => {
           console.log('Request failed');
         });
-    };
-    this.setAgency = function (a) {
-      agency = a;
-    };
-    this.setDate = function (d) {
-      date = d;
-    };
+  };
+  this.setAgency = function (a) {
+    agency = a;
+  };
+  this.setDate = function (d) {
+    date = d;
+  };
 
-    this.setFireType = function (t) {
-      fireType = t;
-    };
-    this.getDate = function () {
-      return date;
-    };
-    this.getFireType = function () {
-      return fireType;
-    };
-    this.getAgency = function () {
-      return agency;
-    };
+  this.setFireType = function (t) {
+    fireType = t;
+  };
+  this.getDate = function () {
+    return date;
+  };
+  this.getFireType = function () {
+    return fireType;
+  };
+  this.getAgency = function () {
+    return agency;
   };
 })
-  .factory('FiresFact', ( $http, FireQuery ) => {
+  .factory('FiresFact', ($http, FireQuery) =>
     // Return an api object
-    return {
-      get (params, cb) {
-        let Fire = new FireQuery();
-        Fire.setAgency(params.src);
-        Fire.setDate(params.date);
+     ({
+       get(params, cb) {
+         const Fire = new FireQuery();
+         Fire.setAgency(params.src);
+         Fire.setDate(params.date);
         // Fire.setFireType(params.fireType);
         // debugger;
-        Fire.getFires(( data ) => {
-          cb(data);
-        });
-      },
-    };
-  }).factory('StringsFact', () => {
-    let strings = {
-      provinces: [
+         Fire.getFires((data) => {
+           cb(data);
+         });
+       },
+     })).factory('StringsFact', () => {
+       const strings = {
+         provinces: [
         { name: 'Manitoba', key: 'MB' },
         { name: '', key: '' },
         { name: 'Ontario', key: 'ON' },
@@ -79,12 +76,12 @@ angular.module('mapprojectApp').service('FireQuery', ($http) => {
         { name: 'Yukon', key: 'YT' },
         { name: 'PC-BA', key: 'PC-BA' },
 
-      ],
-    };
-    return {
+        ],
+       };
+       return {
 
-      getStrings () {
-        return strings;
-      },
-    };
-  });
+         getStrings() {
+          return strings;
+        },
+       };
+     });
